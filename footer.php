3a0a738e22378e96b27ff31babe102f2909a05a4
mradermaker <hallo@monika-radermaker.de>
2025-09-17 15:48:58 +0200

    added footer content and nav

--- footer.php
+++ footer.php
@@ -11,21 +11,30 @@
 
 ?>
 
-	<footer class="c-footer">
-		<nav id="footer-nav" class="c-footer__nav c-footer-nav" aria-label="<?php echo esc_html_e('Footer-Navigation', 'mr',); ?>" role="navigation">
-			<?php wp_nav_menu([
-				'theme_location'	=> 'footer-nav',
-				'menu_class'		=> 'c-footer-nav__list',
-				'container'			=> false,
-				'before'			=> '',
-				'after'				=> '',
-				'link_before'		=> '',
-				'link_after'		=> '',
-				'items_wrap'		=>'<ul id="%1$s" class="%2$s" role="menu">%3$s</ul>',
-				'walker'			=> '',
-			]); ?>
-		</nav>
-	</footer>
+    <footer class="c-footer">
+        <div class="c-footer__container o-container">
+            <div class="c-footer__row o-row --position-center">
+                <div class="c-footer__top o-col-12">
+                    <p class="c-footer__copyright">
+                        <a href="#main">monika-radermaker.de</a> &copy; <?php echo date('Y',); ?>
+                    </p>
+                    <nav id="footer-nav" class="c-footer__nav c-footer-nav" aria-label="<?php echo esc_html_e('Footer-Navigation', 'mr',); ?>" role="navigation">
+                        <?php wp_nav_menu([
+                            'theme_location'	=> 'footer-nav',
+                            'menu_class'		=> 'c-footer-nav__list',
+                            'container'			=> false,
+                            'before'			=> '',
+                            'after'				=> '',
+                            'link_before'		=> '',
+                            'link_after'		=> '',
+                            'items_wrap'		=>'<ul id="%1$s" class="%2$s" role="menu">%3$s</ul>',
+                            'walker'			=> '',
+                        ]); ?>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </footer>
 
 <?php wp_footer(); ?>
 
