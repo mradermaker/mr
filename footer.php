ac4f60b01ff350858b29069903b313a048604543
mradermaker <hallo@monika-radermaker.de>
2025-10-18 19:41:47 +0200

    added footer content

--- footer.php
+++ footer.php
@@ -13,25 +13,32 @@
 
     <footer class="c-footer">
         <div class="c-footer__container o-container">
-            <div class="c-footer__row o-row --position-center">
-                <div class="c-footer__top o-col-12">
-                    <p class="c-footer__copyright">
-                        <a href="#main">monika-radermaker.de</a> &copy; <?php echo date('Y',); ?>
-                    </p>
-                    <nav id="footer-nav" class="c-footer__nav c-footer-nav" aria-label="<?php echo esc_html_e('Footer-Navigation', 'mr',); ?>" role="navigation">
-                        <?php wp_nav_menu([
-                            'theme_location'	=> 'footer-nav',
-                            'menu_class'		=> 'c-footer-nav__list',
-                            'container'			=> false,
-                            'before'			=> '',
-                            'after'				=> '',
-                            'link_before'		=> '',
-                            'link_after'		=> '',
-                            'items_wrap'		=>'<ul id="%1$s" class="%2$s" role="menu">%3$s</ul>',
-                            'walker'			=> '',
-                        ]); ?>
-                    </nav>
+            <div class="c-footer__top">
+                <div class="c-footer__logo u-position-relative">
+                    <span class="c-header__text">MR</span>
+                    <div class="c-handwritten --footer">
+                        <?php get_icon('handwritten-arrow-down', true, ['class' => 'c-handwritten__icon --arrow-down',]); ?>
+                        <span class="c-handwritten__text">Logo <span class="u-screen-reader-only">(Platzhalter)</span></span>
+                    </div>
                 </div>
+            </div>
+            <div class="c-footer__bottom">
+                <p class="c-footer__copyright">
+                    monika-radermaker.de &copy; <?php echo date('Y',); ?>
+                </p>
+                <nav id="footer-nav" class="c-footer__nav c-footer-nav" aria-label="<?php echo esc_html_e('Footer-Navigation', 'mr',); ?>" role="navigation">
+                    <?php wp_nav_menu([
+                        'theme_location'	=> 'footer-nav',
+                        'menu_class'		=> 'c-footer-nav__list',
+                        'container'			=> false,
+                        'before'			=> '',
+                        'after'				=> '',
+                        'link_before'		=> '',
+                        'link_after'		=> '',
+                        'items_wrap'		=>'<ul id="%1$s" class="%2$s" role="menu">%3$s</ul>',
+                        'walker'			=> '',
+                    ]); ?>
+                </nav>
             </div>
         </div>
     </footer>
