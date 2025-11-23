9fd8d66eb4bf78a5fc78121719de3382ab9e4c0d
mradermaker <hallo@monika-radermaker.de>
2025-11-23 20:50:04 +0100

    changed archive template

--- archive.php
+++ archive.php
@@ -12,39 +12,58 @@ get_header();
 
 	<main id="main" class="o-main">
 
-		<?php if ( have_posts() ) : ?>
+		<section class="c-portfolio o-section o-container">
+            <div class="c-portfolio__row o-row --position-center">
+                <div class="c-portfolio__content o-col-12 o-col-xl-8">
+                    <p class="c-portfolio__subheadline c-subheadline">Portfolio</p>
+                    <h1 class="c-portfolio__headline c-headline"><?php echo get_the_archive_title(); ?></h1>
+                </div>
+            </div>
+            
+            <?php
+			$category = get_queried_object();
+            $args = array(
+                'post_type'      => 'post',
+                'posts_per_page' => -1,
+                'tag__not_in' => array(8),
+				'tax_query'      => array(
+					array(
+						'taxonomy' => 'category',
+						'field'    => 'term_id',
+						'terms'    => $category->term_id,
+					),
+				),
+            );
+            $blog_query = new WP_Query( $args );
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+            if ($blog_query->have_posts() ) :
+                $total_posts = (int) $blog_query->found_posts;
+                ?>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+                <div class="c-portfolio__navigation">
+                    <?php printf(
+                    '<p class="c-portfolio__status u-screen-reader-only" aria-live="polite">%s</p>',
+                    sprintf(
+                        _n('%s Eintrag angezeigt', '%s Einträge angezeigt', $total_posts),
+                        $total_posts
+                    )
+                    );?>
+                </div>
+                
+                <div id="portfolio-grid" class="c-portfolio__cards o-row --position-center" role="list">
+                    <?php
+                        while ( $blog_query->have_posts() ) : $blog_query->the_post();
+                            get_template_part( 'template-parts/content', get_post_type(), ['role' => 'listitem'] );
+                        endwhile;
+                    ?>
+                </div>
+                <?php
+                wp_reset_postdata();
+            endif;
+            ?>
+        </section>
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
+	</main>
 
 <?php
 get_footer();
