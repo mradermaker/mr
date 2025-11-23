850ecb39fb9fb4bdc5384337864742c44769b30d
mradermaker <hallo@monika-radermaker.de>
2025-11-23 17:41:58 +0100

    added 404 page

--- 404.php
+++ 404.php
@@ -11,50 +11,19 @@ get_header();
 ?>
 
 	<main id="main" class="o-main">
-
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'mr' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'mr' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'mr' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$mr_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'mr' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$mr_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
+		<section class="c-text">
+			<div class="c-text__container o-section o-container">
+				<div class="c-text__row o-row">
+					<div class="c-text__content o-col-12 o-col-xl-8">
+						<p class="c-text__subheadline c-subheadline">Seite nicht gefunden</p>
+						<h1 class="c-text__headline c-headline">Fehler 404</h1>
+						<p class="c-text__text c-wysiwyg">Diese Seite kann nicht gefunden werden. Es scheint als hätten Sie einen nicht mehr aktuellen Link aufgerufen.</p>
+						<a class="c-text__button c-button" href="<?php echo esc_url(home_url('/')); ?>">Zum Portfolio</a>
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
 
 <?php
 get_footer();
