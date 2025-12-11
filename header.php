fa8a2e15b9489ec476b7710c611dc53b2dbb4775
mradermaker <hallo@monika-radermaker.de>
2025-12-11 19:38:56 +0100

    removed hide header

--- header.php
+++ header.php
@@ -8,9 +8,6 @@
  *
  * @package mr
  */
-
-$hide_header_pages = array(MR_IMPRINT_ID, MR_DATA_PROTECTION_ID);
-$hide_header = is_page(MR_LOGIN_ID) || ( is_page($hide_header_pages) && !is_user_logged_in() );
 ?>
 
 <!doctype html>
@@ -39,47 +36,44 @@ $hide_header = is_page(MR_LOGIN_ID) || ( is_page($hide_header_pages) && !is_user
 </head>
 
 <body <?php body_class('o-body'); ?>>
-    <?php if (!$hide_header) { ?>
-        <a class="c-skip-link u-screen-reader-only" href="#menu-haupt">Zur Navigation</a>
-    <?php } ?>
+
+    <a class="c-skip-link u-screen-reader-only" href="#menu-haupt">Zur Navigation</a>
 	<a class="c-skip-link u-screen-reader-only" href="#main">Zum Inhalt</a>
     <a class="c-skip-link u-screen-reader-only" href="#footer">Zum Footer</a>
 
     <?php wp_body_open(); ?>
 
-	<?php if (!$hide_header) { ?>
-		<header class="c-header">
-			<div class="c-header__container o-container">
-            	<div class="c-header__row o-row">
-                	<div class="c-header__content o-col-12">
-						<div class="c-header__logo u-position-relative">
-                            <a class="c-header__logo-link" href="<?php echo esc_url(home_url('/')); ?>"><span class="c-header__logo-text">MR</span></a>
-                            <div class="c-handwritten --logo">
-                                <?php get_icon('handwritten-arrow-straight-short', true, ['class' => 'c-handwritten__icon',]); ?>
-                                <span class="c-handwritten__text">Logo <span class="u-screen-reader-only">(Platzhalter)</span></span>
-                            </div>
+    <header class="c-header">
+        <div class="c-header__container o-container">
+            <div class="c-header__row o-row">
+                <div class="c-header__content o-col-12">
+                    <div class="c-header__logo u-position-relative">
+                        <a class="c-header__logo-link" href="<?php echo esc_url(home_url('/')); ?>"><span class="c-header__logo-text">MR</span></a>
+                        <div class="c-handwritten --logo">
+                            <?php get_icon('handwritten-arrow-straight-short', true, ['class' => 'c-handwritten__icon',]); ?>
+                            <span class="c-handwritten__text">Logo <span class="u-screen-reader-only">(Platzhalter)</span></span>
                         </div>
-
-                        <nav id="main-nav" class="c-header__nav c-main-nav" aria-label="Haupt-Navigation">
-                            <button class="c-main-nav__button c-icon-button" aria-controls="menu-haupt" aria-expanded="false">
-                                <?php echo get_icon('menu', true, ['class' => 'c-main-nav__button-icon c-icon-button__icon --open']); ?>
-                                <?php echo get_icon('close', true, ['class' => 'c-main-nav__button-icon c-icon-button__icon --close']); ?>
-                                <span class="c-main-nav__button-label c-icon-button__text u-screen-reader-only" data-open-text="Hauptmenü öffnen" data-close-text="Hauptmenü schließen">Hauptmenü öffnen</span>
-                            </button>
-                            <?php wp_nav_menu([
-                                'theme_location'	=> 'main-nav',
-                                'menu_class'		=> 'c-main-nav__list',
-                                'container'			=> false,
-                                'before'			=> '',
-                                'after'				=> '',
-                                'link_before'		=> '',
-                                'link_after'		=> '',
-                                'items_wrap'		=>'<ul id="%1$s" class="%2$s">%3$s</ul>',
-                                'walker'			=> '',
-                            ]); ?>
-                        </nav>
                     </div>
+
+                    <nav id="main-nav" class="c-header__nav c-main-nav" aria-label="Haupt-Navigation">
+                        <button class="c-main-nav__button c-icon-button" aria-controls="menu-haupt" aria-expanded="false">
+                            <?php echo get_icon('menu', true, ['class' => 'c-main-nav__button-icon c-icon-button__icon --open']); ?>
+                            <?php echo get_icon('close', true, ['class' => 'c-main-nav__button-icon c-icon-button__icon --close']); ?>
+                            <span class="c-main-nav__button-label c-icon-button__text u-screen-reader-only" data-open-text="Hauptmenü öffnen" data-close-text="Hauptmenü schließen">Hauptmenü öffnen</span>
+                        </button>
+                        <?php wp_nav_menu([
+                            'theme_location'	=> 'main-nav',
+                            'menu_class'		=> 'c-main-nav__list',
+                            'container'			=> false,
+                            'before'			=> '',
+                            'after'				=> '',
+                            'link_before'		=> '',
+                            'link_after'		=> '',
+                            'items_wrap'		=>'<ul id="%1$s" class="%2$s">%3$s</ul>',
+                            'walker'			=> '',
+                        ]); ?>
+                    </nav>
                 </div>
             </div>
-		</header>
-	<?php } ?>
+        </div>
+    </header>
